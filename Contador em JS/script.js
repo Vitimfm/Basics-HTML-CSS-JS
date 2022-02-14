52e032d24c559eced6f7a327a8299e332fd95fac
Vitor Freitas <96491301+Vitimfm@users.noreply.github.com>
2022-02-14 16:39:20 -0300

    Add files via upload

---
+++ Contador em JS/script.js
@@ -0,0 +1,28 @@
+var currentNumberWraper = document.getElementById('currentNumber');
+var acabar = document.getElementById('acabar');
+var currentNumber = 0;
+var limite = 10;
+function increment() {
+    currentNumber++;
+    currentNumberWraper.innerHTML = currentNumber;
+}
+function decrement() {
+    currentNumber--;
+    currentNumberWraper.innerHTML = currentNumber;
+}
+function Zerar(){
+    currentNumberWraper.innerHTML =  currentNumber = 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+    
